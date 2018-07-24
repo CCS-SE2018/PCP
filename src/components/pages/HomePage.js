a5d0cefce6a9d0ca50fb319b82a07096ed390610
Dan <danjoaquinchin@gmail.com>
2018-07-24 13:26:32 +0800

    more designs

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -29,7 +29,7 @@ class HomePage extends Component {
 
     render() {
         return (
-        <div style={{backgroundColor:'black', color : 'yellow'}}>
+        <div>
             <Column 
                 horizontal='center' 
                 flexGrow={1}>
