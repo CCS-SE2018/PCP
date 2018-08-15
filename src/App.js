4c81454f63a048ac246c93438dae3846f994b57c
Doodyo123 <GamemasterD@users.noreply.github.com>
2018-08-15 16:21:29 +0800

    for sign up

--- src/App.js
+++ src/App.js
@@ -7,25 +7,30 @@ import HomePage from './components/pages/HomePage'
 class App extends Component {
 
   state ={
-    products: [],
-    product: {
-      productName: 'sample product',
-      productPrice: 20
+    user: {
+      userName: '',
+      userPassword: '',
+      firstName: '',
+      lastName: ''
     },
     nextID : '',
+    users: [],
   }
 
+
+
 componentDidMount() {
-  this.getProducts();
+  this.getUsers();
+  console.log("ComponentDidMount");
 }
 
 // retreives the productName from table product
-getProducts = _ => {
-  console.log("getProducts");
-  fetch('http://localhost:4000/products')
+getUsers = _ => {
+  console.log("getUsers");
+  fetch('http://localhost:4000/users')
   .then(response => response.json())
   .then(response => {
-    this.setState({ products: response.data})
+    this.setState({users: response.data});
     this.getMaxID();
   })
   .catch(err => console.error(err))
@@ -33,50 +38,93 @@ getProducts = _ => {
 
 getMaxID(){
   var max = -1;       //gets -1 as a temporary max ID
-  console.log(this.state.products);
-  var products = this.state.products; //saves the products array into a variable
-  var size = products.length; //saves the length of the products array into a variable
+  console.log(this.state.users);
+  var users = this.state.users; //saves the products array into a variable
+  var size = this.state.users.length; //saves the length of the products array into a variable
   for( var i=0 ; i < size ; i++){ // for loop to traverse the array
-    var tempMax = products[i].productID;  //assigns the current productID to a variable (tempMax)
+    var tempMax = this.state.users[i].userID;  //assigns the current productID to a variable (tempMax)
     if(tempMax>max){ //if tempMax is greater than the current max,
       max = tempMax;  //max is now the temp max
     }
   }
-
   max++;  //we increment max by 1 (to avoid conflicts in product IDs in the database)
   console.log(max);
   this.setState({ nextID : max });  //sets the state of nextID to the maximum (with the increment)
 }
+/*
+setProductAvailability(){
+  var defaultValue = 1;
+  this.setState({nextProductAvailability : defaultValue}); //sets the state of nextProductAvailability to the default value
+}
+*/
 
-// Adds products to the database
-addProduct = _ => {
-    const  product  = this.state.product;
-    const productID = this.state.nextID;
-    console.log(product);
-    fetch(`http://localhost:4000/products/add?productID=${productID}&productName=${product.productName}&productPrice=${product.productPrice} `)
+/*
+setSupermarketID(){
+  var defaultValue = 1;
+  this.setState({nextSupermarketID : defaultValue});  //sets the state of nextSupermarketID to the default value
+}
+*/
+
+// Adds users to the database
+addUser = _ => {
+    const  user  = this.state.user;
+    const userID = this.state.nextID;
+    console.log(user);
+    fetch(`http://localhost:4000/users/add?userID=${userID}&userName=${user.userName}&userPassword=${user.userPassword}&firstName=${user.firstName}&lastName=${user.lastName} `)
     .then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err))
 }
 
-  renderProduct =({ productID, productName}) => <div key={productID}>{productName}</div>
+// Adds user to the database
+addProduct = _ => {
+    const  product  = this.state.product;
+    const productID = this.state.nextID;
+    const supermarketID = this.state.nextSupermarketID;
+    const productAvailability = this.state.nextProductAvailability;
+    console.log(product);
+    fetch(`http://localhost:4000/products/add?productID=${productID}&productName=${product.productName}&productPrice=${product.productPrice}&productAvailability=${productAvailability}&supermarketID=${supermarketID} `)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err))
+}
+
+  renderUser =({ userID, userName}) => <div key={userID}>{userName}</div>
 
   render() {
-    const { products, product } = this.state;
+    const { users, user } = this.state;
     return (
       <div className="App">
-        {products.map(this.renderProduct)}
+        {users.map(this.renderUser)}
 
         <div>
+        <br />
+        <p> Username</p>
           <input
-              value = {product.productName}
-              onChange={e => this.setState({ product: { ...product, productName: e.target.value}})}
+              value = {user.userName}
+              onChange={e => this.setState({ user: { ...user, userName: e.target.value}})}
           />
+          <br />
+          <p>Password</p>
           <input
-              value = {product.productPrice}
-              onChange={e => this.setState({ product: { ...product, productPrice: e.target.value}})}
+              value = {user.userPassword}
+              type = "password"
+              onChange={e => this.setState({ user: { ...user, userPassword: e.target.value}})}
           />
-          <button onClick={this.addProduct}>Add product </button>
+          <br />
+          <p>First Name </p>
+          <input
+              value = {user.firstName}
+              onChange={e => this.setState({ user: { ...user, firstName: e.target.value}})}
+          />
+          <br />
+          <p>Last Name </p>
+          <input
+              value = {user.lastName}
+              onChange={e => this.setState({ user: { ...user, lastName: e.target.value}})}
+          />
+          <br />
+          <button onClick={this.addUser}>Register </button>
         </div>
       </div>
     );
