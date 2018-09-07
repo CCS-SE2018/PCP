3de3a1d919355c383ec964c2f820e9e2e97d93e0
Dan <danjoaquinchin@gmail.com>
2018-09-07 15:03:43 +0800

    updates

--- reactexpress/index.js
+++ reactexpress/index.js
@@ -9,6 +9,8 @@ const SELECT_ALL_SUPERMARKET_QUERY = 'SELECT * FROM supermarket';
 const SELECT_ALL_USER_QUERY ='SELECT userID, userName, firstName, lastName FROM user';
 const SELECT_ALL_FEEDBACK_QUERY = 'SELECT * FROM feedback';
 
+const THE_SECRET = 'nakakapagpabagaBAG';
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -16,23 +18,21 @@ const connection = mysql.createConnection({
   database: 'price_check_program'
 });
 
+var pass;
 // A function that hashes a password
 function hashPassword(candidatePassword){
-  bcrypt.genSalt(10, function (err, salt) {
+  const salt = 10;
+
+  bcrypt.hash(candidatePassword, salt, function(err, hash) {
     if (err) {
       return console.log(err);
     }
-    bcrypt.hash(candidatePassword, salt, function (err, hashedPassword) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(hashedPassword);
-    });
+    pass = hash;
   });
 }
 
-//tests the hash function
-hashPassword('password');
+// //tests the hash function
+// const pass = hashPassword('password');
 
 connection.connect(err => {
   if (err) {
@@ -71,18 +71,59 @@ app.get('/products/add', (req, res) => {
 app.get('/users/add', (req, res) => {
   const { userID, userName, userPassword, firstName, lastName } = req.query;
   console.log(req.query);
-  const INSERT_USERS_QUERY = `INSERT INTO user (userID, userName, userPassword, firstName, lastName)
-  VALUES (${userID},'${userName}', '${userPassword}', '${firstName}', '${lastName}')`;
-  console.log(INSERT_USERS_QUERY);
-  connection.query(INSERT_USERS_QUERY, (err, results) => {
+  
+  const salt = 10;
+  bcrypt.hash(userPassword, salt, function(err, hash) {
     if (err) {
-      return res.send(err);
-    } else {
-      return res.send('successfully added user');
+      return console.log(err);
     }
+    const INSERT_USERS_QUERY = `INSERT INTO user (userID, userName, userPassword, firstName, lastName)
+    VALUES (${userID},'${userName}', '${hash}', '${firstName}', '${lastName}')`;
+    connection.query(INSERT_USERS_QUERY, (err, results) => {
+      if (err) {
+        return res.send(err);
+      } else {
+        return res.send(results);
+      }
+    });
   });
 });
 
+//auth for user
+app.get('/users/auth', (req, res) => {
+  const { userName, userPassword } = req.query;
+
+  const GET_USERS_QUERY = `SELECT userID, userName, userPassword, isAdmin, firstName, lastName FROM user
+  WHERE userName = '${userName}'`;
+  connection.query(GET_USERS_QUERY, (err, results) => {
+    if (err) {
+      return res.send(err);
+    } 
+    try{
+      bcrypt.compare(userPassword, results[0].userPassword, function(err, isCorrect) {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json({
+          auth : isCorrect,
+          admin : results[0].isAdmin,
+          user : {
+            firstName : results[0].firstName,
+            lastName : results[0].lastName,
+            userName : results[0].userName,
+          }
+        });
+      });
+    }catch(e){
+      return res.json({
+        auth : false,
+        admin : false,
+        message: 'an error occurred!',
+        error : e
+      });
+    }
+  });
+});
 //adds data to supermarket
 app.get('/supermarkets/add', (req, res) => {
   const { supermarketID, supermarketName, supermarketAddress } = req.query;
@@ -100,7 +141,6 @@ app.get('/supermarkets/add', (req, res) => {
 
 // gets all products in the database
 const productQuery = 'SELECT productID, productName, productPrice, productAvailability, s.supermarketName FROM product p INNER JOIN supermarket s WHERE p.supermarketID = s.supermarketID';
-
 app.get('/products', (req, res) => {
   connection.query(productQuery, (err, results) => {
     if (err) {
@@ -114,7 +154,7 @@ app.get('/products', (req, res) => {
 });
 
 // gets all products with the same name in the database
-app.get('/product/getProduct', (req, res) => {
+app.get('/products/getProduct', (req, res) => {
   const SEARCH_A_PRODUCT_NAME = `SELECT productName FROM product WHERE productName ='Apple'`;
   connection.query(SEARCH_A_PRODUCT_NAME, (err, results) => {
     if (err) {
